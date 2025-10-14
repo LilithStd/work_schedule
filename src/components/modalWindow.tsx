d01005821f360829e8d53a89cc23c54f623d4fa1
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-10-14 13:23:31 +0300

    add input and save button to modal window (now without logic)

--- src/components/modalWindow.tsx
+++ src/components/modalWindow.tsx
@@ -13,16 +13,18 @@ export default function ModalWindow({ isOpen, onClose, children }: ModalWindowPr
             onClick={onClose}
         >
             <div
-                className="bg-white rounded-2xl p-6 shadow-lg max-w-sm w-full"
+                className="bg-white rounded-2xl p-6 shadow-lg max-w-sm w-full gap-2"
                 onClick={(e) => e.stopPropagation()}
             >
                 {children}
+                <input type="text" className="border border-gray-300 rounded-md p-2 w-full" placeholder="Введите текст..." />
                 <button
                     onClick={onClose}
-                    className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 w-full"
+                    className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 w-1/2"
                 >
                     Закрыть
                 </button>
+                <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 w-1/2">Save</button>
             </div>
         </div>
     );
