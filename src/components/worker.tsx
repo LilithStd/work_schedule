21aa2048748c39bd83a56107fe395d92b4885957
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-10-18 22:01:55 +0300

    work on drag systems (for worker list by day)

--- src/components/worker.tsx
+++ src/components/worker.tsx
@@ -1,13 +1,21 @@
 interface WorkerProps {
     name?: string;
     id: string;
+    callBack?: (id: string) => void;
 }
 
-export default function Worker({ name }: WorkerProps) {
+export default function Worker({ name, id, callBack }: WorkerProps) {
     return (
         <div
             className="cursor-pointer bg-violet-600 m-1 rounded text-white"
-            draggable
+            draggable="true"
+            onDragStart={(e) => {
+                console.log('drag start', id);
+                e.dataTransfer.setData("text/plain", id);
+                // if (callBack) {
+                //     callBack(id);
+                // }
+            }}
         >
             <p>
                 {name ? name : 'worker'}
--- src/components/workersList.tsx
+++ src/components/workersList.tsx
@@ -1,29 +1,51 @@
 'use client';
 import { nanoid } from 'nanoid';
-import React from 'react'
+import React, { useState } from 'react'
 import Worker from './worker';
+import { week } from '@/consts/template';
+
+type WorkersListProps = {
+    day: string;
+    workers?: string[];
+}
 
 export default function WorkersList() {
+    const [workerDragged, setWorkerDragged] = useState('');
+    const [workerListByDay, setWorkerListByDay] = useState<WorkersListProps[]>([]);
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        console.log('drag over');
+    }
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>, day: string) => {
+        e.preventDefault();
+        setWorkerListByDay({ ...workerListByDay, [day]: [...(workerListByDay.find(item => item.day === day)?.workers || []), workerDragged] });
+        console.log('drop end', day, workerDragged);
+    }
     return (
         <>
             <div className="border border-black bg-sky-600 text-center justify-center">
                 <h2 className="text-center">workers</h2>
                 {Array.from({ length: 4 }).map(() => {
                     const id = nanoid();
-                    return <Worker key={id} id={id} />;
+                    return <Worker key={id} id={id} callBack={setWorkerDragged} />;
                 })}
 
             </div>
-            {Array.from({ length: 7 }).map((_, index) => (
+            {week.map((_, index) => (
                 <div
                     onDrop={() => {
-                        console.log('drop end')
+                        console.log('drop end +');
                     }}
-                    onDragOver={() => console.log('drag over')}
+                    onDragEnd={() => {
+                        console.log('drag end');
+                    }}
+                    onDragOver={handleDragOver}
                     key={index}
                     className="border border-black bg-sky-600"
                 >
-
+                    {workerListByDay.find(item => item.day === week[index])?.workers?.map((workerId) => (
+                        <p key={workerId}>{workerId}</p>
+                    ))}
                 </div>
             ))}
         </>
