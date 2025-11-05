e63c4d4a890bc18e43473f3afff891e082917c7e
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-11-05 14:57:15 +0200

    commit before global change visual view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import TableCell from "@/components/tableCell";
 export default function Main() {
   return (
     <main className="align-center flex min-h-screen w-full flex-col">
-      <div>Main Page</div>
       <TableCell />
     </main>
   )
