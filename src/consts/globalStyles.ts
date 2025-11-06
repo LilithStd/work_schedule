10b3875030d677e397f35a58797a8376ec5363e7
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-11-06 23:45:59 +0200

    add template for one style for app

---
+++ src/consts/globalStyles.ts
@@ -0,0 +1,25 @@
+export const shadow = {
+	button: '',
+	text: '',
+	container: '',
+	title: '',
+};
+
+export const indents = {
+	button: {
+		margin: 'm-2',
+		padding: 'p-2',
+	},
+	text: {
+		margin: 'm-2',
+		padding: 'p-2',
+	},
+	container: {
+		margin: 'm-2',
+		padding: 'p-2',
+	},
+	title: {
+		margin: 'm-2',
+		padding: 'p-2',
+	},
+};
