eac09d218d53d18bb0a6ec4a7d5c9e5389875d47
Dmitrii <dmitrii.konevnin@gmail.com>
2025-12-28 23:12:02 +0200

    update styles for schedule page (dark  theme)

--- src/components/workerDataModalTemplate.tsx
+++ src/components/workerDataModalTemplate.tsx
@@ -217,11 +217,11 @@ export default function WorkerDataModalTemplate({ onClose, typeWorkerModal, work
                                 </div>
                                 <div className="flex gap-2">
                                     <button
-                                        className="mt-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 w-1/2"
+                                        className={`mt-4 py-2 rounded-xl ${THEME_COLORS[currentThemeApp].hover} w-1/2`}
                                         onClick={handleResetState}
                                     >Reset</button>
                                     <button
-                                        className="mt-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 w-1/2"
+                                        className={`mt-4 py-2 rounded-xl ${THEME_COLORS[currentThemeApp].button} ${THEME_COLORS[currentThemeApp].hover} w-1/2`}
                                         onClick={() => handleEndChangeWorkerData()}
                                     >Save</button>
                                 </div>
