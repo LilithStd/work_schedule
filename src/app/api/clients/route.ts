35d33d14f903188b61f3bae5cdae384000491f12
Dmitrii <dmitrii.konevnin@gmail.com>
2026-01-07 22:29:30 +0200

    add base api for change data between pages

---
+++ src/app/api/clients/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+
+const  clients = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Anna' }
+]
+
+// GET — получить клиентов
+export async function GET() {
+  return NextResponse.json(clients)
+}
+
+// POST — создать клиента
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  const newClient = {
+    id: Date.now(),
+    name: body.name,
+  }
+
+  clients.push(newClient)
+
+  return NextResponse.json(newClient, { status: 201 })
+}
